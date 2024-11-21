afb3a7d515de5b01f0a79bb27a060a35b2d909be
Artem Larionenko <lrrrtm.edu@yandex.ru>
2024-11-22 02:30:54 +0300

    github webhook test

--- git_deployer_server/server.py
+++ git_deployer_server/server.py
@@ -1,42 +1,39 @@
-from flask import Flask, request
+from fastapi import FastAPI, Request
 from dotenv import load_dotenv
-import os
 
+import uvicorn
+import os
 import telebot
 
 load_dotenv()
 bot = telebot.TeleBot(os.getenv('BOT_API_KEY'))
 
-app = Flask(__name__)
 
 
-@app.route('/github-webhook/', methods=['POST'])
-def webhook():
-    if request.method == 'POST':
-        mk = telebot.types.InlineKeyboardMarkup()
-        button = telebot.types.InlineKeyboardButton(text="Перезапуск", callback_data="git_pull")
-        mk.add(button)
-        text = "Получено обновление с GitHub. Чтобы перезапустить, нажми на кнопку"
-
-        try:
-            bot.send_message(
-                chat_id=os.getenv('ADMIN_1_ID'),
-                text=text,
-                reply_markup=mk
-            )
-
-            bot.send_message(
-                chat_id=os.getenv('ADMIN_2_ID'),
-                text=text,
-                reply_markup=mk
-            )
-        except Exception as e:
-            print(e)
-            pass
-
-        return 'Success', 200
-    return 'Invalid method', 405
+app = FastAPI()
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+@app.post("/github-webhook/")
+async def github_webhook():
+    mk = telebot.types.InlineKeyboardMarkup()
+    button = telebot.types.InlineKeyboardButton(text="Перезапуск", callback_data="git_pull")
+    mk.add(button)
+    text = "Получено обновление с GitHub. Чтобы перезапустить, нажми на кнопку"
+
+    try:
+        bot.send_message(
+            chat_id=os.getenv('ADMIN_1_ID'),
+            text=text,
+            reply_markup=mk
+        )
+
+        bot.send_message(
+            chat_id=os.getenv('ADMIN_2_ID'),
+            text=text,
+            reply_markup=mk
+        )
+    except Exception as e:
+        print(e)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=5000)
